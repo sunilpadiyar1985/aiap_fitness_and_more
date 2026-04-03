3662ffaae14dd4dd5840a5b2b10b8991110f47d0
sunilpadiyar1985 <sunilpadiyar1985@gmail.com>
2026-04-03 15:32:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
 import streamlit as st
+
+# Redirect notice page
+st.markdown("""
+    <meta http-equiv="refresh" content="5;url=https://unbrokentribe-fitness.streamlit.app/">
+    <div style="
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 80vh;
+        text-align: center;
+        font-family: sans-serif;
+    ">
+        <h2>🚀 We've moved on! To a bigger/better place. </h2>
+        <p>This app is no longer available at this URL.</p>
+        <p>You will be redirected to the new app in <b>5 seconds</b>.</p>
+        <p>If not, <a href="https://unbrokentribe-fitness.streamlit.app/">click here</a>.</p>
+    </div>
+""", unsafe_allow_html=True)
+
+st.stop()
+
 import pandas as pd
 import plotly.express as px
 import numpy as np
